225aae001ac3b77f9fd3aa217c472466ddbcfb19
Adam Kiovský <adamkiovsky@seznam.cz>
2020-12-06 20:12:23 +0100

    Skupiny, rozdělení skupin

---
+++ app/src/main/java/com/example/dluznicek/CustomListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.dluznicek;
+
+public class CustomListAdapter {
+}
---
+++ app/src/main/java/com/example/dluznicek/currentGroup.java
@@ -0,0 +1,14 @@
+package com.example.dluznicek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class currentGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_group);
+    }
+}
